3bd31fd7115667e37062912c397552e7b7c08a55
yyoonchul <yiyc02@naver.com>
2024-08-07 15:23:06 +0900

    에러 페이지 추가

--- leadliner/__init__.py
+++ leadliner/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
@@ -6,6 +6,11 @@ from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+def page_not_found(e):
+    return render_template('404.html'), 404
+def page_not_able(e):
+    return render_template('500.html'), 500
 
 def create_app():
     app = Flask(__name__)
@@ -18,8 +23,8 @@ def create_app():
         migrate.init_app(app, db, render_as_batch=True)
     else:
         migrate.init_app(app, db)
+    
     from . import models
-
 
     from .views import main_views
     from .views import auth_views
@@ -29,6 +34,7 @@ def create_app():
     app.register_blueprint(auth_views.bp)
     app.register_blueprint(mypage_views.bp)
     app.register_blueprint(onboarding_views.bp)
-
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, page_not_able)
 
     return app
