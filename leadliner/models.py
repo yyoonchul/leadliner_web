ec00a7014026fa0980accd2f4a53c7d8f37f3e65
yyoonchul <yiyc02@naver.com>
2024-08-19 15:40:16 +0900

    모델 오토 인크레멘트

--- leadliner/models.py
+++ leadliner/models.py
@@ -1,18 +1,18 @@
 from leadliner import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     join_date = db.Column(db.DateTime(), nullable=False)
 
 class UserKeywordData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     uid = db.Column(db.Integer)
     keyword_list = db.Column(db.Text(), nullable=False)
 
 class PreSetKeywordData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     description = db.Column(db.String(200), nullable=False)
     keyword_list = db.Column(db.Text(), nullable=False)
