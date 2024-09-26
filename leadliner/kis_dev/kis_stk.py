8c4ff0db8e119143dfc1e5669dad2fae7024859e
yyoonchul <yiyc02@naver.com>
2024-09-26 17:26:43 +0900

    뉴스 링크 디자인 개선

--- leadliner/kis_dev/kis_stk.py
+++ leadliner/kis_dev/kis_stk.py
@@ -45,3 +45,31 @@ def get_overseas_price_quot_price(excd="", itm_no="", tr_cont="", dataframe=None
     dataframe = current_data
 
     return dataframe
+
+
+#미국 주식 주간 시세 이슈 해결 시 사용할 수 있는 함수
+# def get_overseas_price_quot_dailyprice(excd="", itm_no="", gubn="", bymd="", modp="0", tr_cont="", dataframe=None):
+#     url = '/uapi/overseas-price/v1/quotations/dailyprice'
+#     tr_id = "HHDFS76240000" # 해외주식 기간별시세
+
+#     if bymd is None:
+#         bymd = datetime.today().strftime("%Y%m%d")   # 종료일자 값이 없으면 현재일자
+
+#     params = {
+#         "AUTH": "",             # (사용안함) 사용자권한정보
+#         "EXCD": excd,           # 거래소코드 	HKS : 홍콩,NYS : 뉴욕,NAS : 나스닥,AMS : 아멕스,TSE : 도쿄,SHS : 상해,SZS : 심천,SHI : 상해지수
+#                                 #           SZI : 심천지수,HSX : 호치민,HNX : 하노이,BAY : 뉴욕(주간),BAQ : 나스닥(주간),BAA : 아멕스(주간)
+#         "SYMB": itm_no,         # 종목번호
+#         "GUBN": gubn,           # 일/주/월구분 0:일. 1:주, 2:월
+#         "BYMD": bymd,           # 조회기준일자(YYYYMMDD) ※ 공란 설정 시, 기준일 오늘 날짜로 설정
+#         "MODP": modp,           # 수정주가반영여부 0 : 미반영, 1 : 반영
+#         "KEYB": ""              # (사용안함) NEXT KEY BUFF  응답시 다음값이 있으면 값이 셋팅되어 있으므로 다음 조회시 응답값 그대로 셋팅
+#     }
+#     res = kis._url_fetch(url, tr_id, tr_cont, params)
+
+#     # Assuming 'output' is a dictionary that you want to convert to a DataFrame
+#     current_data = pd.DataFrame(res.getBody().output2)
+
+#     dataframe = current_data
+
+#     return dataframe
