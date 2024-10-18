3af7d77c98281dd2484b8b51e2cce64a4ee2be54
yyoonchul <yiyc02@naver.com>
2024-10-18 14:39:04 +0900

    sec

--- config/production.py
+++ config/production.py
@@ -11,7 +11,7 @@ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     db=os.getenv('DB_NAME'))
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b':\x0e\xba\x86\x8b\xed\xe7dc\xe2\x16\xef\xbd+\n='
+SECRET_KEY = #SECRET_KEY here
 
 dictConfig({
     'version': 1,
